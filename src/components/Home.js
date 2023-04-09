b1deb612dc69b45a6d727ca1095f69bb1a6e7b6b
Rayyan <rayyanaslam16@gmail.com>
2023-04-10 00:26:35 +0530

    Implemented Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import image from "../img5.jpg"
 
 export default function Home(){
     return(
     <>
-        <h1>Home Page</h1>
-    </>)
+        <main>
+            <img src={image} alt="Wallpaper" className="absolute object-center w-full h-full" />
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-6xl text-cyan-800 font-bold cursive leading-none lg:leading-snug home-name">Aloha. I'm Perseus</h1>
+            </section>
+        </main>
+    </>
+    )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { SocialIcon } from "react-social-icons";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
 
 export default function NavBar() {
   return (
     <>
-      <header className="bg-red-600">
+      <header className="bg-teal-800">
         <div className="container mx-auto flex justify-between">
           <nav className="flex">
             <NavLink
@@ -15,8 +13,8 @@ export default function NavBar() {
               exact
               className={(navData) =>
                 navData.isActive
-                  ? "inline-flex items-center py-6 px-3 mr-4 text-white hover:text-green-800 text-4xl font-bold cursive tracking-widest"
-                  : "inline-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest"
+                  ? "inline-flex items-center py-6 px-3 mr-4 text-white hover:text-green-300 text-4xl font-bold cursive tracking-widest"
+                  : "inline-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-300 text-4xl font-bold cursive tracking-widest"
               }
             >
               Perseus
@@ -25,8 +23,8 @@ export default function NavBar() {
               to="/post"
               className={(navData) =>
                 navData.isActive
-                  ? "inline-flex items-center py-3 px-3 my-6 rounded text-red-100 bg-red-700 hover:text-green-800"
-                  : "inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+                  ? "inline-flex items-center py-3 px-3 my-6 rounded text-red-100 bg-cyan-900 hover:text-green-300"
+                  : "inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-300"
               }
             >
               Blog Posts
@@ -35,8 +33,8 @@ export default function NavBar() {
               to="/project"
               className={(navData) =>
                 navData.isActive
-                  ? "inline-flex items-center py-3 px-3 my-6 rounded text-red-100 bg-red-700 hover:text-green-800"
-                  : "inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+                  ? "inline-flex items-center py-3 px-3 my-6 rounded text-red-100 bg-cyan-900 hover:text-green-300"
+                  : "inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-300"
               }
             >
               Projects
@@ -45,8 +43,8 @@ export default function NavBar() {
               to="/about"
               className={(navData) =>
                 navData.isActive
-                  ? "inline-flex items-center py-3 px-3 my-6 rounded text-red-100 bg-red-700 hover:text-green-800"
-                  : "inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+                  ? "inline-flex items-center py-3 px-3 my-6 rounded text-red-100 bg-cyan-900 hover:text-green-300"
+                  : "inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-300"
               }
             >
               About Me!
@@ -54,7 +52,7 @@ export default function NavBar() {
           </nav>
 
           <div className="inline-flex py-3 px-3 my-6">
-          {/* <FontAwesomeIcon icon={faHashnode} style={{color: "#1a5ad1",}} /> */}
+            {/* <FontAwesomeIcon className="rounded-full p-2 mr-4" icon={faHashnode} size="xl" style={{ color: "#3b5e9b" }} /> */}
             <SocialIcon
               url="https://github.com/rayyan016"
               className="mr-4"
@@ -71,18 +69,22 @@ export default function NavBar() {
             />
             <SocialIcon
               url="https://hashnode.com/@RayyanAslam"
+              network=""
               className="mr-4"
               target="_blank"
               fgColor="#fff"
               style={{ height: 35, width: 35 }}
             />
           </div>
-          
         </div>
       </header>
     </>
   );
 }
 
-// className={(navData) => navData.isActive ? "active" : "" }
-// className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+// npm i --save @fortawesome/fontawesome-svg-core
+//npm i --save @fortawesome/react-fontawesome@latest
+//npm i --save @fortawesome/free-brands-svg-icons
+
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faHashnode } from "@fortawesome/free-brands-svg-icons";
