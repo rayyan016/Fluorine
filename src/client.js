1209d24694a4b938d8192cea59a142855fe4b62d
Rayyan <rayyanaslam16@gmail.com>
2023-04-12 01:20:00 +0530

    Fixed bug in Post.js

--- src/client.js
+++ src/client.js
@@ -1,6 +1,17 @@
-import { SanityClient } from "@sanity/client";
+// import sanityClient from "@sanity/client";
 
-export default SanityClient({
-    projectId: "lponqi1p",
-    dataset: "production"
+// export default sanityClient({
+//     projectId: "lponqi1p",
+//     dataset: "production"
+// })
+
+import {createClient} from '@sanity/client'
+
+const client = createClient({
+  projectId: "lponqi1p",
+  dataset: "production",
+  useCdn: false, // set to `true` to fetch from edge cache
+
 })
+
+export default client;
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,12 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import SanityClient from "../client.js";
+import client from "../client.js";
+
+
+
 
 export default function Post() {
   const [postData, setPost] = useState(null);
 
+
+
   useEffect(() => {
-    SanityClient.fetch(
+
+    client.fetch(
       `*[_type == "post"]{
             title,
             slug,
@@ -34,19 +40,31 @@ export default function Post() {
             Welcome to my page of blog posts
           </h2>
           <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-            <article>
-              <Link to={"/post/" + post.slug.current} key={post.slug.current}>
-                <span
-                  className="block h-64 relative shadow leading-snug bg-white border-l-8 border-green-800"
-                  key={index}
-                >
-                  <img src="" alt="" />
-                  <span>
-                    <h3></h3>
-                  </span>
-                </span>
-              </Link>
-            </article>
+            {postData &&
+              postData.map((post, index) => (
+                <article>
+                  <Link
+                    to={"/post/" + post.slug.current}
+                    key={post.slug.current}
+                  >
+                    <span
+                      className="block h-64 relative shadow leading-snug bg-white border-l-8 border-green-800"
+                      key={index}
+                    >
+                      <img
+                        src={post.mainImage.asset.url}
+                        alt={post.mainImage.alt}
+                        className="w-full h-full rounded-r object-cover absolute"
+                      />
+                      <span className=" relative h-full flex justify-end items-end pr-4 pb-4">
+                        <h3 className=" text-lg font-bold px-3 py-4 bg-red-700 text-red-100 bg-opacity-75 rounded">
+                          {post.title}
+                        </h3>
+                      </span>
+                    </span>
+                  </Link>
+                </article>
+              ))}
           </div>
         </section>
       </main>
