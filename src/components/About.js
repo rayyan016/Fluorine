65c79dd845cc1351d95d2ae69e3bc39e3739aa82
Rayyan <rayyanaslam16@gmail.com>
2023-04-15 23:23:26 +0530

    Implemented about component

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import client from "../client.js";
+import bgimage from "../bgimg1.jpg";
+import imageUrlBuilder from "@sanity/image-url";
+import SanityBlockContent from "@sanity/block-content-to-react";
 
-export default function About(){
-    return(
+const builder = imageUrlBuilder(client);
+function urlFor(source) {
+  return builder.image(source);
+}
+
+export default function About() {
+  const [author, setAuthor] = useState(null);
+
+  useEffect(() => {
+    client
+      .fetch(
+        `*[_type == "author"]{
+            name,
+            bio,
+            "authorImage": image.asset->url
+        }`
+      )
+      .then((data) => setAuthor(data[0]))
+      .catch(console.error);
+  }, []);
+
+  if (!author) return <div>Loading...</div>;
+  return (
     <>
-        <h1>About Page</h1>
-    </>)
+      <main className="relative">
+        <img src={bgimage} alt="background" className="absolute w-full" />
+        <div className="p-10 lg:pt-48 container mx-auto relative">
+          <section className="bg-teal-800 rounded-lg shadow-2xl lg:flex p-20">
+            <img
+              src={urlFor(author.authorImage).url()}
+              alt={author.name}
+              className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8"
+            />
+            <div className="text-lg flex flex-col justify-center">
+              <h1 className="cursive text-6xl text-green-300 mb-4">
+                Hey there. I'm{" "}
+                <span className="text-green-100">{author.name}</span>
+              </h1>
+              <div className="prose lg:prose-xl text-white">
+                <SanityBlockContent
+                  blocks={author.bio}
+                  projectId="lponqi1p"
+                  dataset="production"
+                ></SanityBlockContent>
+              </div>
+            </div>
+          </section>
+        </div>
+      </main>
+    </>
+  );
 }
